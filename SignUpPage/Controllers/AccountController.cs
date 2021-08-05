3fe62f5a5668bae2fa02b8b00a9af4a85f94a83b
91997 <91997@LAPTOP-I40C18KD>
2021-08-05 13:47:28 +0530

    Add project files.

---
+++ SignUpPage/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignUpPage.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+
+namespace SignUpPage.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+#pragma warning disable IDE0060 // Remove unused parameter
+        public IActionResult SignUp(UserModel model)
+#pragma warning restore IDE0060 // Remove unused parameter
+        {
+          
+
+            if (ModelState.IsValid)
+            {
+                // TO DO:
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+    }
+}
---
+++ SignUpPage/Models/UserModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using SignUpPage.Attributes;
+
+namespace SignUpPage.Models
+{
+    public class UserModel
+    {
+        [Required(ErrorMessage = "Please Enter Name.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Username.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Password.")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Confirm Password.")]
+        [Compare("Password",ErrorMessage = "Confirm Password Doesn't Match.")]
+        public string ConfirmPassword { get; set; }
+
+        [RegularExpression ("^[6789]\\d{9}$", ErrorMessage ="Please Enter Correct Contact Number.")]
+        public string Contact { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Country.")]
+        public string Country { get; set; }
+
+        [Required(ErrorMessage = "Please Select City.")]
+        public string City { get; set; }
+   
+        [Required(ErrorMessage = "Please Select Gender.")]
+        public string Gender { get; set; }
+
+        [ValidateCheckBox(ErrorMessage = "Please Accept Terms.")]
+        public bool Terms { get; set; }
+    }
+
+
+    
+}
